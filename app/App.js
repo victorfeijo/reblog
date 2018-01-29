88942d1e90c183df1f5e3230a483ca0fd03a9941
Victor Feijo <victor.feijoa@gmail.com>
2018-01-28 23:36:22 -0200

    Adds antd design with sass styling

--- app/App.js
+++ app/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
+import { Layout } from 'antd';
+
+import './App.scss';
+
+const { Header, Content, Footer } = Layout;
+
 const App = (props) => (
-  <div>
-    Application root
-  </div>
+  <Layout>
+    <Header className="header">
+      Info about blog
+    </Header>
+    <Content className="content">
+      <div className="content__post">
+        <h1>Blog title</h1>
+        blog content
+      </div>
+    </Content>
+    <Footer className="footer">
+      Feijo blog ©2016
+    </Footer>
+  </Layout>
 );
 
 export default App;
